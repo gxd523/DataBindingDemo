9fa06c9be5180d157c9929322319a1f3bec0b88f
gxd <guoxiaodong@haqu.com>
2020-11-04 11:08:36 +0800

    no message

---
+++ app/src/main/java/com/demo/aac/AACActivity.java
@@ -0,0 +1,63 @@
+package com.demo.aac;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+
+import com.demo.aac.databinding.ActivityAacBinding;
+
+import java.util.concurrent.TimeUnit;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
+
+/**
+ * DataBinding+ViewModel+LiveData
+ */
+public class AACActivity extends BaseActivity {
+    private ActivityAacBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_aac);
+        // 加了这句就不需要设置liveData.observe()了
+        binding.setLifecycleOwner(this);
+        final LiveViewModel viewModel = ViewModelProviders.of(this).get(LiveViewModel.class);
+        binding.setViewModel(viewModel);
+        viewModel.liveData.setValue("aaa");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                viewModel.liveData.postValue("bbb");
+            }
+        }).start();
+
+        binding.activityAacTitleTv.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("gxd", "ViewModelActivity.onTextChanged-->" + s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        binding.getViewModel().liveData.setValue("ccc");
+    }
+}
