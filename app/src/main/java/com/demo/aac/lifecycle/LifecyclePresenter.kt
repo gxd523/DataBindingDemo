02101dcff6d2fc66664f1aab7299a6077c1d2e4f
gxd <gxd523@gmail.com>
2021-03-26 11:39:42 +0800

    no message

---
+++ app/src/main/java/com/demo/aac/lifecycle/LifecyclePresenter.kt
@@ -0,0 +1,45 @@
+package com.demo.aac.lifecycle
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+import kotlin.concurrent.thread
+
+/**
+ * Created by guoxiaodong on 3/26/21 10:52
+ */
+class LifecyclePresenter(private val viewer: LifecycleUseActivity) : LifecycleObserver {
+    fun getDataOnStart(owner: LifecycleOwner) {
+        requestData { result ->
+            if (owner.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+                viewer.runOnUiThread {
+                    viewer.onRequestData(result)
+                }
+            }
+        }
+    }
+
+    private fun requestData(block: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            block("get data!")
+        }
+    }
+
+    @OnLifecycleEvent(value = Lifecycle.Event.ON_START)
+    fun onStart() {
+        Log.d("gxd", "MyObserver.onStart-->")
+    }
+
+    @OnLifecycleEvent(value = Lifecycle.Event.ON_STOP)
+    fun onStop() {
+        Log.d("gxd", "MyObserver.onStop-->")
+    }
+
+    @OnLifecycleEvent(value = Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        Log.d("gxd", "MyObserver.onDestroy-->")
+    }
+}
--- app/src/main/java/com/demo/aac/lifecycle/LifecycleUseActivity.kt
+++ app/src/main/java/com/demo/aac/lifecycle/LifecycleUseActivity.kt
@@ -1,13 +1,23 @@
 package com.demo.aac.lifecycle
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
 
 /**
  * Created by guoxiaodong on 3/26/21 10:51
  */
-class LifecycleUseActivity : LifecycleActivity() {
+class LifecycleUseActivity : ComponentActivity() {
+    private val presenter by lazy {
+        LifecyclePresenter(this).also(lifecycle::addObserver)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycle.addObserver(MyObserver())
+        presenter.getDataOnStart(this)
+    }
+
+    fun onRequestData(result: String) {
+        Log.d("gxd", "LifecycleUseActivity.onRequestData-->${result}")
     }
 }
